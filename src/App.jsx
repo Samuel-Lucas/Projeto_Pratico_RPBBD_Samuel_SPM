d57bdf0f9d8d313d43a3236e841808a09b889821
Samuel-Lucas <samuel.moreira10@outlook.com>
2020-11-16 16:38:47 -0300

    Adicionando Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import './App.css'
 
 import Header from './componentes/Header'
@@ -11,7 +12,15 @@ import Produtos from './paginas/Produtos'
 export default _ =>
           <div>
             <Header />
-             <PaginaInicial />
+              <Switch>
+                <Route exact path="/">
+                  <Redirect to="/Index" />
+                </Route>
+                <Route path="/Index" component={PaginaInicial}/>
+                <Route path="/Produtos" component={Produtos}/>
+                <Route path="/Lojas" component={Lojas}/>
+                <Route path="/Fale-Conosco" component={FaleConosco}/>
+              </Switch>
             <Footer />
           </div>
   
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     
@@ -8,9 +9,9 @@ export default function Header() {
 
                 <div className="container">
 
-                    <a href="pagina_inicial.html" className="logo">
+                    <Link to="/Index" className="logo">
                         <h3>Games<br /><i>Shelter</i></h3>
-                    </a>
+                    </Link>
 
                         <button className="navbar-toggler" data-toggle="collapse" data-target="#navPrincipal">
                             <span className="navbar-toggle-icon">
@@ -21,15 +22,15 @@ export default function Header() {
                         <div className="collapse navbar-collapse" id="navPrincipal">
                             <ul className="navbar-nav ml-auto">
                                 <li className="nav-item menu_item" onmouseover="mudaEstilo()">
-                                    <a href="produtos.html"><h4>Produtos</h4></a>
+                                    <Link to="/Produtos"><h4>Produtos</h4></Link>
                                 </li>
 
                                 <li className="nav-item menu_item" onmouseover="mudaEstilo()">
-                                    <a href="lojas.html"><h4>Lojas</h4></a>
+                                    <Link to="/Lojas"><h4>Lojas</h4></Link>
                                 </li>
 
                                 <li className="nav-item menu_item" onmouseover="mudaEstilo()">
-                                    <a href="fale_conosco.html"><h4>Fale conosco</h4></a>
+                                    <Link to="/Fale-Conosco"><h4>Fale conosco</h4></Link>
                                 </li>
                             </ul>
                         </div>
